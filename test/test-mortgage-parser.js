f0a6e7e0ced7f5ecb6c43cafa6f1a780db82ecb3
Сидоров Владимир Александрович <Sidorov.V.Ale@sberbank.ru>
2018-08-31 14:20:09 +0300

    Test refactoring.

--- test/test-mortgage-parser.js
+++ test/test-mortgage-parser.js
@@ -1,55 +1,21 @@
 const fs = require('fs');
-
 const mortgageParser = new (require('./../index').MortgageParser)();
+const CASE_DIR = './test/cases/';
 
-// ZipFile
-let data = fs.readFileSync('./test/cases/case_noticeReleaseMortgage_4.zip');
-let res = mortgageParser.parseZip(data, '0');
-console.log(res);
+// File pattern description: case_<request.requestType>_<caseNum>.<fileExtension>
 
-// ZipFile
-data = fs.readFileSync('./test/cases/case_noticeReleaseMortgage_2.zip');
-res = mortgageParser.parseZip(data, '0');
-console.log(res);
+console.log('Begin tests!');
 
-// ZipFile
-data = fs.readFileSync('./test/cases/case_noticeReleaseMortgage_3.zip');
-res = mortgageParser.parseZip(data, '0');
-console.log(res);
-
-// ZipFile
-data = fs.readFileSync('./test/cases/case_directionAgreement_2.zip');
-res = mortgageParser.parseZip(data, '0');
-console.log(res);
-
-// ZipFile
-data = fs.readFileSync('./test/cases/case_directionAgreement_1.zip');
-res = mortgageParser.parseZip(data, '0');
-console.log(res);
-
-// ZipFile
-data = fs.readFileSync('./test/cases/case_noticeReleaseMortgage_1.zip');
-res = mortgageParser.parseZip(data, '0');
-console.log(res);
-
-// ZipFile
-data = fs.readFileSync('./test/cases/case_checkingInformationOwner_1.zip');
-res = mortgageParser.parseZip(data, '0');
-console.log(res);
-
-
-// ZipFile
-data = fs.readFileSync('./test/cases/case_NoticeRedemption_1.zip');
-res = mortgageParser.parseZip(data, '0');
-console.log(res);
-
-// ZipFile
-data = fs.readFileSync('./test/cases/case_transferElectronicMortgageDepositary_1.zip');
-res = mortgageParser.parseZip(data, '0');
-console.log(res);
-
-
-
+fs.readdirSync(CASE_DIR).forEach(fileName => {
+    if (fileName.match(/(^case)(_\w*_)\d*\.(zip|txt)$/igms)) {
+        const fileData = fs.readFileSync('./test/cases/case_noticeReleaseMortgage_4.zip');
+        const fileResultParse = mortgageParser.parseZip(fileData, '0');
+        console.log(fileResultParse);
+    }
+    else {
+        console.log('Incorrect test case file name: ' + fileName);
+    }
+});
 
 // DataUrl
 data = fs.readFileSync('./test/cases/case_noticeReleaseMortgage_dataUrl_1.txt', 'utf8');
