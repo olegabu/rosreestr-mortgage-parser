87f61a5d97976a6922b1f4b2ce593cd569d6dee6
Сидоров Владимир Александрович <Sidorov.V.Ale@sberbank.ru>
2018-09-12 10:51:44 +0300

    Add support Jsonschema validation.

--- mortgage-validator.js
+++ mortgage-validator.js
@@ -8,24 +8,21 @@ const RequestSchema = JSON.parse(fs.readFileSync('./mappings/Request.jsonschema'
 const TRequestJsonSchema = JSON.parse(fs.readFileSync('./mappings/TRequest.jsonschema').toString());
 
 let ajv = new Ajv({
-  schemaId: 'auto',
-//    meta: false,
+    //schemaId: '$id',
+    coerceTypes: true,
+    allErrors: true,
+    $data: true,
+    verbose: true
 });
 
-const metaSchema = require('./node_modules/ajv/lib/refs/json-schema-draft-06.json');
-ajv.addMetaSchema(metaSchema);
-//ajv._opts.defaultMeta = metaSchema.id;
-//ajv._refs['http://json-schema.org/schema'] = 'http://json-schema.org/draft-04/schema';
-ajv.addSchema(XMLSchemaJsonSchema, 'http://www.jsonix.org/jsonschemas/w3c/2001/XMLSchema.jsonschema');
-ajv.addSchema(JsonixJsonSchema, 'http://www.jsonix.org/jsonschemas/jsonix/Jsonix.jsonschema');
-ajv.addSchema(RequestSchema, 'urn://x-artefacts-rosreestr-gov-ru/virtual-services/electronic-mortgage/1.0.0');
+//const metaSchema = require('./node_modules/ajv/lib/refs/json-schema-draft-07.json');
+//ajv.addMetaSchema(metaSchema);
+ajv.addSchema(XMLSchemaJsonSchema, 'http://www.jsonix.org/jsonschemas/w3c/2001/XMLSchema.jsonschema#');
+ajv.addSchema(JsonixJsonSchema, 'http://www.jsonix.org/jsonschemas/jsonix/Jsonix.jsonschema#');
+//ajv.addSchema(RequestSchema, 'urn://x-artefacts-rosreestr-gov-ru/virtual-services/electronic-mortgage/1.0.0');
 //ajv.addSchema(RequestSchema, 'urn://x-artefacts-rosreestr-gov-ru/virtual-services/electronic-mortgage/1.0.0');
 const validate = ajv.compile(MortgageJsonSchema);
 const po = JSON.parse(fs.readFileSync("test/mortgage.json").toString());
 
 const valid = validate(po);
-if (!valid) {
-    console.log('Validation failed.');
-    console.log('Validation errors:');
-    console.log(validate.errors);
-}
+console.log(valid);
